08f3becc6126df726c7637d76f942fe114957341
Ap73MKa <artyom.vlasov.05@mail.ru>
2023-02-26 11:28:38 +0300

    ADD: image optimizer

--- app/frontend/react-app/vite.config.ts
+++ app/frontend/react-app/vite.config.ts
@@ -1,12 +1,15 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), ViteImageOptimizer()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
