01facb78b08972c8fd1cb25658fd6e9ae987b351
Ap73MKa <artyom.vlasov.05@mail.ru>
2023-05-04 13:05:39 +0300

    ADD: download qr code

--- react-client/src/components/LinksComponents/QRGenerator.tsx
+++ react-client/src/components/LinksComponents/QRGenerator.tsx
@@ -1,17 +1,32 @@
 'use client'
 
 import { useQRCode } from 'next-qrcode'
+import { useRef } from 'react'
 
 const QR_GENERATOR_OPTIONS = {
   margin: 3,
   width: 200
 }
-
 export default function QRGenerator({ hrefLink }: { hrefLink: string }) {
   const { Canvas } = useQRCode()
+  const qrCodeContainerRef = useRef<HTMLDivElement>(null)
+
+  const handleDownloadQRCode = () => {
+    const canvas = qrCodeContainerRef.current?.querySelector(
+      'canvas'
+    ) as HTMLCanvasElement | null
+    if (!canvas) return
+    const image = canvas.toDataURL('image/png')
+    const link = document.createElement('a')
+    link.download = 'qr-code.png'
+    link.href = image
+    link.click()
+  }
   return (
-    <div className="cursor-pointer overflow-hidden rounded-xl">
-      <Canvas text={hrefLink} options={QR_GENERATOR_OPTIONS} />
-    </div>
+    <button type="button" onClick={handleDownloadQRCode}>
+      <div className="overflow-hidden rounded-xl" ref={qrCodeContainerRef}>
+        <Canvas text={hrefLink} options={QR_GENERATOR_OPTIONS} />
+      </div>
+    </button>
   )
 }
