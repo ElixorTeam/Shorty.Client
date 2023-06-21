b0fca5525fa77e5a350730c0291a0381b3c911a8
Thienlao17 <danil.klimenko.vlad@mail.ru>
2023-06-21 20:50:07 +0300

    FIX: react-redirect

--- react-redirect/src/utils/api.ts
+++ react-redirect/src/utils/api.ts
@@ -6,4 +6,4 @@ export const API_URL = 'http://localhost:8082/shorty/api/v1'
 export const getLinkByInnerRef = async (
   innerRef: string
 ): Promise<ExternalRefResponseType> =>
-  ky.get(`${API_URL}/external_ref_by_inner/${innerRef}`).json()
+  ky.get(`${API_URL}/links/external_ref_by_inner/${innerRef}`).json()
