2fa7f9bf4d1f94c47def285b087452147af2e1a1
Ap73MKa <artyom.vlasov.05@mail.ru>
2023-06-28 21:28:25 +0300

    UPDATE: env config for docker

--- react-client/src/shared/urls.ts
+++ react-client/src/shared/urls.ts
@@ -1,2 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL
-export const REDIRECT_URL = process.env.NEXT_PUBLIC_REDIRECT_URL
+export const API_URL = `${process.env.NEXT_PUBLIC_BACKEND_DOMAIN}/shorty/api/v1`
+export const REDIRECT_URL = `${process.env.NEXT_PUBLIC_REDIRECT_DOMAIN}`
--- react-redirect/src/utils/api.ts
+++ react-redirect/src/utils/api.ts
@@ -1,7 +1,8 @@
 import ky from 'ky'
 import { ExternalRefResponseType } from '@/shared/ExternalRefResponseType'
 
-export const API_URL = import.meta.env.VITE_API_URL
+const ENV_API = import.meta.env.VITE_BACKEND_DOMAIN
+export const API_URL = `${ENV_API}/shorty/api/v1`
 
 export const getLinkByInnerRef = async (
   innerRef: string,
