5de33472f618104a7b932e3e29a36ce8bdbacc25
Ap73MKa <artyom.vlasov.05@mail.ru>
2023-06-28 15:49:09 +0300

    UPDATE: docker compose, package

--- react-client/next.config.js
+++ react-client/next.config.js
@@ -1,4 +1,4 @@
-const withNextIntl = require('next-intl/plugin')('./i18n.ts')
+const withNextIntl = require('next-intl/plugin')('./src/i18n.ts')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withNextIntl({
